f37544c506b46a8833a62372a1e28c47918a2957
Anderson Parra <ander.pp@gmail.com>
2013-04-01 15:11:13 -0300

    theads with concurrent api

---
+++ concurrent/executor.rb
@@ -0,0 +1,18 @@
+include Java
+
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+service = Executors.new_fixed_thread_pool 10
+
+for i in 1..20
+  service.execute {
+    start = Time.new
+    puts "Processando..."
+    puts "Tempo: #{Time.new - start}"
+  }
+end
+
+service.shutdown
+service.await_termination 30, TimeUnit::SECONDS
